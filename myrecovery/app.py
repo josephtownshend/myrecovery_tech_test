1c1849afadf02707c080b974a3b9c985683b9ed2
joseph townshend <joetownshend@mac.com>
2019-10-01 11:19:06 +0100

    feat: team array set up in memory and get_team function working and returning JSON data

--- myrecovery/app.py
+++ myrecovery/app.py
@@ -1,6 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+team = [
+  {
+    "profilePicture": "https://www.profilePictures.com/mary-edwards-walker",
+    "firstName": "Mary",
+    "lastName": "Edwards Walker",
+    "type": "Surgeon",
+    "onLeave": "false",
+    "specialities": [
+    "Orthopaedics",
+    "Renal"
+    ],
+    "biography": "American abolitionist, prohibitionist, prisoner of war and surgeon."
+  },
+  {
+    "profilePicture": "https://www.profilePictures.com/florence-nightingale",
+    "firstName": "Florence",
+    "lastName": "Nightingale",
+    "type": "Nurse",
+    "onLeave": "false",
+    "specialities": [
+    "Orthopaedics",
+    "Renal",
+    "Paediatrics"
+    ],
+    "biography": "English social reformer and statistician, and the founder of modern nursing."
+  },
+  {
+    "profilePicture": "https://www.profilePictures.com/joan-rivers",
+    "firstName": "Joan",
+    "lastName": "Rivers",
+    "type": "Admin Assistant",
+    "onLeave": "false",
+    "specialities": [
+    "Orthopaedics",
+    ],
+    "biography": "American comedian, actress, writer, producer, and television host."
+  }
+]
+
+@app.route('/myrecovery/api/v1.0/team', methods=['GET'])
+def get_team():
+    return jsonify({'team': team})
+
+if __name__ == '__main__':
+    app.run(debug=True)
