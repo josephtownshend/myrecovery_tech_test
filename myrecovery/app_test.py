7e46e3329ec18ecb2d74e0fa8fdc141a963920ff
joseph townshend <joetownshend@mac.com>
2019-10-01 19:16:35 +0100

    feat: app_test can return team

--- myrecovery/app_test.py
+++ myrecovery/app_test.py
@@ -4,11 +4,26 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+team = [
+  {
+    "profilePicture": "https://www.profilePictures.com/mary-edwards-walker",
+    "firstName": "Mary",
+    "lastName": "Edwards Walker",
+    "type": "Surgeon",
+    "onLeave": "false",
+    "specialities": [
+    "Orthopaedics",
+    "Renal"
+    ],
+    "biography": "American abolitionist, prohibitionist, prisoner of war and surgeon."
+  }
+]
 
-api.add_resource(HelloWorld, '/')
+class Team(Resource):
+    def get(self):
+        return {'team': team}
+
+api.add_resource(Team, '/myrecovery/api/v1.0/team')
 
 if __name__ == '__main__':
     app.run(debug=True)
